730de54c29a3dff986665ef7f04914a51b2dbedf
sabill <sabila.hadinn245@gmail.com>
2018-11-30 17:20:35 +0700

    sabill

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php 
 include('server.php');
-session_start();
-$_SESSION["username"] = $username;
 ?>
 
 <!DOCTYPE html>
@@ -10,8 +8,8 @@ $_SESSION["username"] = $username;
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-show-password/1.0.3/bootstrap-show-password.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-show-password/1.0.3/bootstrap-show-password.min.js"></script>
 
   <head>
     <div class="header">
